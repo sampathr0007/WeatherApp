57f6298d6f98610178c06d0b38058bd8ff0da465
Gondesi Sampath Kumar Reddy <72519849+sampathr0007@users.noreply.github.com>
2024-05-06 23:43:35 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "9d6a65afd667868b4101338725df32f0",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
